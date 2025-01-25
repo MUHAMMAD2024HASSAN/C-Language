c3ef10dbfc0f3898cbe560dc23033ac9a374bb5d
Muhammad Hassan <muhammadnasssah@gmail.com>
2025-01-25 22:00:21 +0500

    first commit

---
+++ start.c
@@ -0,0 +1,243 @@
+#include <stdio.h>
+#include <math.h>
+int main() {
+printf("Hello Hassan \n");
+// printf("Hello Hassan \n");
+// printf("Hello Hassan \n");
+// printf("Hello Hassan \n");
+// int age = 18;
+// printf("My age is %d \n",age);
+// float width = 20;
+// printf("%f \n",width);
+// char star = '*';
+// printf("%c \n",star);
+// int age;
+// printf("Enter age");
+// scanf("%d", &age);
+// printf("age is %d", age);
+// int  a , b;
+// printf("enter a");
+// scanf("%d" ,&a);
+// printf("enter b");
+// scanf("%d" ,&b);
+// printf("sum is : %d", a*b);
+//Area of suqare
+// int side;
+// printf("Enter side");
+// scanf("%d" ,&side);
+// printf("Area is : %d" ,side*side);
+// float side;
+// printf("Enter side");
+// scanf("%f" ,&side);
+// printf("Area is : %f" ,side*side);
+// float radius;
+// printf("Enter radius");
+// scanf("%f" ,&radius);
+// printf("Radius is : %f" , 3.142*radius*radius);
+// int a = 18;
+// int b = a;
+// int c = b*6;
+// int d = 1, e;
+// int oldAge = 50;
+// int year = 2;
+// int newAge = oldAge + year;
+// printf("%d" ,16%10);
+// printf("%d" , -8%3)
+// int a = (int) 1.99999;
+// printf("%d" ,a);
+// int a = 4+9*10;
+// printf("%d"  ,a);
+// int a = 4*3/6*2;
+// printf("%d" ,a);
+// printf("%d \n" , 4 != 4); // True
+// printf("%d \n" , 4 == 4); // True (4 == 3 False)
+// printf("%d \n" , 4<5 && 5<6); // Logical Operator (&&)
+// printf("%d \n " , !(4<5)); // Logical Operator (!)
+// printf("%d \n" , !( (4<5) && (5<4) ) ); // Logical Operator (!)
+// int x;
+// printf("enter a number :");
+// scanf("%d", &x);
+// printf("%d", x % 2 == 0);
+// int x; int y = x;
+// int x = printf("Hello");
+// printf("%d \n" , x);
+// int isSunday = 1;
+// int isSnowing = 0;
+// printf("%d \n", isSunday && isSnowing);
+// int x;
+// printf("Enter a number  :");
+// scanf("%d", &x);
+// printf("%d \n", x>0 && x<100);
+// int age;
+// printf("Enter age :");
+// scanf("%d", &age);
+// age >=18 ? printf("Adult \n") : printf("Not Adult!");
+// if ( age >= 18 ) {
+//     printf("Adult \n");
+//     printf("Thay can have a Drive! \n");
+//     printf("Thay can a Vote \n");
+//     printf("Thay can a ??");
+// }
+// else if ( age > 13 && age < 18)
+// {
+//     printf("Teeenager! \n");
+// }
+
+// else {
+//     printf("Child");
+// }
+
+// char day;
+// printf("Enter a valid day (1-7) :");
+// scanf("%s", &day);
+// switch (day)
+// {
+// case 'm': printf("Monday \n");
+//     break;
+
+// case 't': printf("Tuesday \n");
+//     break;
+// case 'w': printf("wednessday \n");
+//     break;
+// case 'T': printf("Thursday \n");
+//     break;
+// case 'f': printf("Friday \n");
+//     break;
+// case 's': printf("Saturday \n");
+//     break;
+
+// case 'S': printf("Sunday \n");
+//     break;
+// default: printf("Not A valid day!");
+//     break;
+// }
+// int number;
+// printf("Enter a number :");
+// scanf("%d", &number);
+// if (number >= 0) {
+//     printf("possitive \n");
+//   if (number % 2 == 0){
+//       printf("Even \n");
+//   }
+//   else{
+//       printf("odd \n");
+//     }
+// }
+// else {
+//     printf("Negative \n");        
+// }
+    
+//     
+// int marks;
+// printf("Enter a marks :");
+// scanf("%d", &marks);
+// if (marks<30){
+//     printf("Grade is C \n");
+// }
+// else if (marks >= 30 && marks < 70) {
+//     printf("Grade is B \n");
+// }
+// else if (marks >= 70 && marks < 90) {
+//     printf("Gread is A \n");
+// }
+// else{
+//     printf("A+ \n");
+// }
+
+// int X = 2;
+// if (X=1){
+// 	printf("X is equal to 1");
+// }
+// else{
+// 	printf("X is not equal to 1");
+// }
+
+// for(int a=1; a<=100; a=a+1){
+// 	printf("%d", a);
+// }
+
+// for(int i=0; i<=10; i++){
+// 	printf("%d", i);
+// }
+// for(char ch='a'; ch<='z'; ch++){
+// 	printf("%c", ch);
+// }
+// int n;
+// printf("Enter a number! :");
+// scanf("%d", &n);
+// int i=0;
+// while (i<=n){
+// 	printf("%d", i);
+// 	i++;
+// }
+// for (int i = 0; i<=n; i++){
+// 	printf("%d", i);
+// }
+
+// int n;
+// printf("Enter a number! :");
+// scanf("%d", &n);
+// int sum=0;
+// for (int i=0, j=n; i<=n && j>=1; i++,j--){
+// 	sum = sum + i; // Sum += i;
+// 	printf("%d\n", j);
+// }
+// printf("sum is %d\n", sum);
+// for(int i=n; i>=1; i--){
+//  printf("%d\n", i);
+// }
+
+// int n;
+// printf("Enter a number :");
+// scanf("%d", &n);
+// for (int i=1; i<=10; i++){
+// 	printf("%d\n", n*i);
+// }
+
+// int n;
+// do{
+// printf("Enter a number :");
+// scanf("%d\n", &n);
+// printf("%d\n", n);
+
+// if (n % 7 == 0 ){ // multiple of 7 
+// 	break;
+// }
+
+// }while(1);
+
+// printf("Thanks !");
+
+// for (int i=0; i<=10; i++){
+// 	if (i==6){ // Skip
+// 		continue;
+// 	}
+	
+// 	printf("%d", i);
+// }
+
+// for (int i=5; i<=50; i++){
+// 	if (i % 2!= 0) {
+// 		printf("%d", i);
+// 	}
+	
+// }
+// int n;
+// printf("Enter a number ! :");
+// scanf("%d", &n);
+// int fact;
+// for (int i=1; i<=n; i++){
+// 	fact = fact * i;	
+// }
+// printf("final Factoral is %d", fact);
+
+int n;
+printf("Enter a number :");
+scanf("%d", &n);
+
+for (int i=10; i>=1; i--){
+	printf("%d", n*i);
+}
+
+return 0;
+}
